cd1d3884a9d0b77f3743bcb0ad1caaacabeedb0d
mirzabekov00 <prog.eldar@gmail.com>
2021-04-02 23:46:42 +0200

    automatically add new resolvers to schema

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,17 @@ import Express from "express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
 
-import { RegisterResolver } from "./modules/user/Register";
-import { LoginResolver } from "./modules/user/Login";
 import session from "express-session";
 import connectRedis from "connect-redis";
 import { redis } from "./redis";
 import cors from "cors";
-import { MeResolver } from "./modules/user/Me";
 import { MyContext } from "./types/MyContext";
-import { ConfirmUserResolver } from "./modules/user/ConfirmUser";
 
 const start = async () => {
   await createConnection();
 
   const schema = await buildSchema({
-    resolvers: [
-      RegisterResolver,
-      LoginResolver,
-      MeResolver,
-      ConfirmUserResolver,
-    ],
+    resolvers: [__dirname + "/modules/**/*.ts"],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
     },
