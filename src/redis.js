bc1df58df001916a3eef4cd318234ce2bb82b7d0
mirzabekov00 <prog.eldar@gmail.com>
2021-04-03 19:15:05 +0200

    send email with change password token

---
+++ src/redis.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+exports.redis = void 0;
+var ioredis_1 = require("ioredis");
+exports.redis = new ioredis_1["default"]();
--- src/utils/createConfirmationUrl.ts
+++ src/utils/createConfirmationUrl.ts
@@ -1,10 +1,11 @@
 import { redis } from "../redis";
 import { v4 } from "uuid";
+import { confirmationPrefix } from "../modules/user/constants/redisPrefixes";
 
 export const createConfirmationUrl = async (userId: number) => {
   const token = v4();
 
-  await redis.set(token, userId, "ex", 60 * 60 * 24); // 1 day expiration
+  await redis.set(confirmationPrefix + token, userId, "ex", 60 * 60 * 24); // 1 day expiration
 
   return `http://localhost:3000/user/confirm/${token}`;
 };
